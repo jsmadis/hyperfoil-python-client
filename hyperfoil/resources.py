21f4f0c36036f48f3416729dcab077297dff026a
jsmadis <jakub.smadis@gmail.com>
2020-08-11 11:54:01 +0200

    Run resource client methods

--- hyperfoil/resources.py
+++ hyperfoil/resources.py
@@ -15,4 +15,15 @@ class Run(DefaultResource):
     def __init__(self, client: DefaultClient = None, entity: dict = None, content_type: str = '',
                  entity_id: str = "") -> None:
         super().__init__(client, entity, content_type, entity_id)
-    # TODO: add methods from clients here
+
+    def kill(self):
+        return self.client.kill(self._entity_id)
+
+    def sessions(self):
+        return self.client.sessions(self._entity_id)
+
+    def recent_sessions(self):
+        return self.client.recent_sessions(self._entity_id)
+
+    def total_sessions(self):
+        return self.client.total_sessions(self._entity_id)

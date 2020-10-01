b6ca4d1ab522d9d9f49777db4bc5143314141d96
jsmadis <jakub.smadis@gmail.com>
2020-10-01 19:19:14 +0200

    Fixed naming of sharedConnection

--- hyperfoil/factories/benchmark_factory.py
+++ hyperfoil/factories/benchmark_factory.py
@@ -20,7 +20,7 @@ class BenchmarkFactory:
     def add_host(self, host: str, shared_connections: int, **kwargs) -> 'BenchmarkFactory':
         self._benchmark['http'].append({
             'host': host,
-            'shared_connections': shared_connections,
+            'sharedConnections': shared_connections,
             **kwargs
         })
         return self
